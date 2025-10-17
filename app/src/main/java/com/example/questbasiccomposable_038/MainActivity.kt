604989d3c6f29ba2a43cd8db2b9e473978cdd6b0
SuperXRanzo <okanskyavenger@gmail.com>
2025-10-17 17:10:09 +0700

    memanggil tata letakboxcolumnrow

--- app/src/main/java/com/example/questbasiccomposable_038/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_038/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestBasicComposable_038Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    TataletakBoxColumnRow(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
